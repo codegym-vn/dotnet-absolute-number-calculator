5661fea31558813fcaae2e65d633a19eb88cb046
nguyenkhanhtung <tungnk0711@gmail.com>
2020-03-10 16:02:37 +0700

    commit

---
+++ AbsoluteCalculator.Service/AbsoluteNumberCalulator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbsoluteCalculator.Service
+{
+    public class AbsoluteNumberCalulator
+    {
+        public int FindAbsolute(int number)
+        {
+            //throw new Exception();
+            if (number < 0)
+                return -number;
+            return number;
+        }
+    }
+}
